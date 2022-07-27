98ca474d6cfb65f4005e8945f26f6be223b49fe4
Vitor Santos <pireshugo737@gmail.com>
2022-07-27 05:05:01 -0300

    refactor: code refactoring & organization (new functions, variables etc.) | add: Funcionality -> Finished Tasks (array / function / checked property)(to be concluded)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,34 @@
-const btnSubmit = document.querySelector("[type='submit']");
-const cardNotify = document.querySelector(".card-notify");
 let tasks = [];
+let finishedTasks = [];
 
-const template = document.querySelector("template");
 const list = document.querySelector(".list-task");
+const template = document.querySelector("template");
 
+// CardNotify & Children's
+const cardNotify = document.querySelector(".card-notify");
+let imgCardNotify = cardNotify.children[0];
+let h3CardNotify = cardNotify.children[1];
+
+// btn
+const btnSubmit = document.querySelector("[type='submit']");
 btnSubmit.addEventListener("click", () => {
   const task = document.querySelector(".task-input");
   let taskDesc = task.value.trim();
-  taskDesc
-    ? addTask(task, taskDesc)
-    : alert("O nome da tarefa não pode ser vazio.");
+
+  taskDesc ? addTask(task, taskDesc) : errorInsertTask();
 });
 
+// add
 function addTask(task, taskDesc) {
   let items = template.content.cloneNode(true);
-  let li = items.querySelector("li");
 
+  let li = items.querySelector("li");
   let cb = items.querySelector("[type='checkbox']");
   let p = items.querySelector(".desc-item");
-  let btn = items.querySelector("button");
+  let btn = items.querySelector(".btn");
 
   p.innerText = taskDesc;
+
   tasks.push(taskDesc);
 
   localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -29,47 +36,49 @@ function addTask(task, taskDesc) {
   li.append(cb, p, btn);
   list.append(li);
 
-  // Card Notify
-  cardNotify.style.background = "#94ff94"
-  cardNotify.children[1].innerText = `Tarefa: "${p.innerText}" Adicionada com Sucesso!`
-  cardNotify.children[0].src = "assets/img/added.png"
-  cardNotify.animate(
-    [
-      { opacity: "0" },
-      { opacity: "1" },
-      {opacity: "0"}
-    ], 
-    {
-      duration: 3000,
-    }
-  );
+  // CardNotify Styles
+  cardNotify.style.background = "#94ff94";
+  imgCardNotify.src = "assets/img/added.png";
+  h3CardNotify.innerText = `Tarefa: "${p.innerText}" Adicionada com Sucesso!`;
+
+  cardAnimate();
 
   // User Interaction
   task.value = "";
   task.focus();
 }
 
+function errorInsertTask() {
+  // CardNotify Styles
+  cardNotify.style.background = "#eb7979";
+  imgCardNotify.src = "assets/img/error.png";
+  h3CardNotify.innerText = `O nome da tarefa não pode ser vazio!`;
+
+  cardAnimate();
+}
+
 function removeTask(item) {
   let remoteIndex = tasks.indexOf(item.children[1].innerHTML); // Índice do elemento removido
-  item.remove(item); // Tela
+  list.removeChild(item); // Tela
   tasks.splice(remoteIndex, 1); // Array
-  
 
-  cardNotify.style.background = "#eb7979"
-  cardNotify.children[1].innerText = `Tarefa: "${item.children[1].innerText}" Removida com Sucesso!`
-  cardNotify.children[0].src = "assets/img/removed.png"
+  // CardNotify Styles
+  cardNotify.style.background = "#eb7979";
+  imgCardNotify.src = "assets/img/removed.png";
+  h3CardNotify.innerText = `Tarefa: "${item.children[1].innerText}" Removida com Sucesso!`;
+
+  cardAnimate();
+  setLocalStorage(tasks);
+}
+
+function cardAnimate() {
   cardNotify.animate(
-    [
-      { opacity: "0" },
-      { opacity: "1" },
-      {opacity: "0"}
-    ], 
+    [{ opacity: "0" }, { opacity: "1" }, { opacity: "0.5" }, { opacity: "0" }],
     {
-      duration: 3000,
+      duration: 5900,
+      fill: "forwards",
     }
   );
-
-  setLocalStorage(tasks);
 }
 
 function setLocalStorage(tasks) {
@@ -98,15 +107,23 @@ function storageTasks() {
 function finishTask(li) {
   let checkbox = li.children[0];
   let p = li.children[1];
-  checkbox.addEventListener("change", (e) => {
-    if (e.target.checked) {
-      p.style.textDecoration = "line-through";
-      p.style.color = "gray";
-    } else {
-      p.style.textDecoration = "none";
-      p.style.color = "black";
-    }
-  });
+
+  // Verifando quando a checkbox está sendo marcada/desmarcada.
+  if (checkbox.checked) {
+    // Styles
+    p.style.textDecoration = "line-through";
+    p.style.color = "gray";
+
+    // Array
+    finishedTasks.push(p.innerText);
+  } else {
+    // Styles
+    p.style.textDecoration = "none";
+    p.style.color = "black";
+
+    // Removendo do Array pelo conteúdo do index
+    finishedTasks.splice(finishedTasks.indexOf(li.children[1].innerHTML), 1);
+  }
 }
 
 storageTasks();
